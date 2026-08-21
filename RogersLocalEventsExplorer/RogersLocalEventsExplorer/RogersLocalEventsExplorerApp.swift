7008207a3dd1b636d7545885f14a29884af4943f
Jayt27 <jt.iosdev@gmail.com>
2026-08-20 21:46:15 -0400

    Initial commit

---
+++ RogersLocalEventsExplorer/RogersLocalEventsExplorer/RogersLocalEventsExplorerApp.swift
@@ -0,0 +1,18 @@
+//
+//  RogersLocalEventsExplorerApp.swift
+//  RogersLocalEventsExplorer
+//
+//  Created by Jay Thakkar on 20/08/26.
+//
+
+import SwiftUI
+
+@main
+struct LocalEventsExplorerApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            EventsListView()
+        }
+    }
+}
---
+++ RogersLocalEventsExplorer/RogersLocalEventsExplorer/View/EventList/EventRow.swift
@@ -0,0 +1,18 @@
+//
+//  EventRow.swift
+//  RogersLocalEventsExplorer
+//
+//  Created by Jay Thakkar on 20/08/26.
+//
+
+import SwiftUI
+
+struct EventRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EventRow()
+}
